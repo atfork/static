8c64adb61d6bff6640ea4bbad5fba4c1f7c54827
San Se <101367491+atfork@users.noreply.github.com>
2023-11-06 22:23:13 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -3,13 +3,13 @@
 set -eux 
 
 
-CITY=Qiyang 
+CITY=qiyang 
 LANGUAGE="zh-CN"
-UNIT=F
+UNIT=Fm
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  https://wttr.in/$CITY?3\&$UNIT
+  https://wttr.in/$CITY?format=4\&$UNIT
